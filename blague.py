04cb11e2ea559f1be75782b9e16c645e6843997f
toukoum <raphaelgiraud12@gmail.com>
2020-11-28 18:10:29 +0100

    blague trop drôle git add blague.py

--- blague.py
+++ blague.py
@@ -1,10 +1,11 @@
-while True:
+boole = True 
+while boole:
     a = input("Vous voulez une blague ?")
-    if a:
+    if a == 'O' or a == 'o':
         print('Quelle est la différence entre un tsunamie et un ricard ?\n')
-        print("Aucune, plus tu rajoute de l'eau et moins il y a de jaune ;) !!!"")
-        False
+        print("Aucune, plus tu rajoute de l'eau et moins il y a de jaune ;) !!!")
+        boole = False
 
     else:
         print('Vous êtes pas drôle !:(')
-        True
+        boole = True
