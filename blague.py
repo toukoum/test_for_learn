092314bfde1a263525f6bbbfc1fb6f5007db5ff9
toukoum <raphaelgiraud12@gmail.com>
2020-11-28 17:56:16 +0100

    while True

--- blague.py
+++ blague.py
@@ -1,3 +1,10 @@
+while True:
+    a = input("Vous voulez une blague ?")
+    if a:
+        print('Quelle est la différence entre un tsunamie et un ricard ?\n')
+        print("Aucune, plus tu rajoute de l'eau et moins il y a de jaune ;) !!!"")
+        False
 
-print('Quelle est la différence entre un tsunamie et un ricard ?\n')
-print("Aucune, plus tu rajoute de l'eau et moins il y a de jaune ;) !!!"")
+    else:
+        print('Vous êtes pas drôle !:(')
+        True
